52f484abc9de599b9926ce6e3978f8603f66789c
Leonard <Leonardhernandez02@gmail.com>
2025-03-29 23:02:37 -0500

    create product service

---
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/services/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.andres.curso.springboot.app.springboot_crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.andres.curso.springboot.app.springboot_crud.entities.Product;
+import com.andres.curso.springboot.app.springboot_crud.repositories.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Product> findAll() {
+        return (List<Product>) productRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Transactional
+    @Override
+    public Optional<Product> delete(Product product) {
+        Optional<Product> productDb = productRepository.findById(product.getId());
+        productDb.ifPresent(p -> productRepository.delete(p));
+        return productDb;
+    }
+
+}
