752f10880d665667776b85884f6eddfe2107bd7a
Leonard <Leonardhernandez02@gmail.com>
2025-04-01 22:34:15 -0500

    add more mapping to product controller

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/controllers/ProductController.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +40,33 @@ public class ProductController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productServiceImpl.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Long id) {
+        Optional<Product> productDb = productServiceImpl.findById(id);
+
+        if (productDb.isPresent()) {
+            product.setId(productDb.get().getId());
+            return ResponseEntity.status(HttpStatus.CREATED).body(productServiceImpl.save(product));
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Product> productDb = productServiceImpl.findById(id);
+
+        if (productDb.isPresent()) {
+            productServiceImpl.delete(productDb.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
