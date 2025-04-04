7d6d2b096ec883de8d3685b09765b6c91ba70cfd
Leonard <Leonardhernandez02@gmail.com>
2025-04-03 21:28:15 -0500

    improve the message of bad request in the update and create end points

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/controllers/ProductController.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package com.andres.curso.springboot.app.springboot_crud.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,27 +48,45 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@Valid @RequestBody Product product) {
+    public ResponseEntity<?> create(@Valid @RequestBody Product product, BindingResult result) {
+        if(result.hasFieldErrors()) {
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(productServiceImpl.save(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@Valid @RequestBody Product product, @PathVariable Long id) {
+    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Product product,
+            BindingResult result) {
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }
         Optional<Product> productOptional = productServiceImpl.update(id, product);
         if (productOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.get());
         }
         return ResponseEntity.notFound().build();
     }
-
+    
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         Optional<Product> productDb = productServiceImpl.findById(id);
-
+        
         if (productDb.isPresent()) {
             productServiceImpl.delete(productDb.get());
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+
     }
 }
--- src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/Product.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -18,7 +18,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
+    @NotBlank
     @Size(min = 3, max = 30)
     private String name;
 
@@ -26,7 +26,7 @@ public class Product {
     @NotNull
     private Double price;
 
-    @NotEmpty
+    @NotBlank
     private String description;
     
     public Long getId() {
