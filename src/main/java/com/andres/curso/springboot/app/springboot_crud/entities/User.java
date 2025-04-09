07dafa4921f355815b910e107be47924e046a383
Leonard Hernandez <leonardhernandez02@gmail.com>
2025-04-08 21:42:44 -0500

    first steps to using spring security

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/User.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
@@ -30,39 +31,48 @@ public class User {
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
             @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
     private List<Role> roles;
-
+    
+    @Transient
+    private boolean admin;
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public List<Role> getRoles() {
         return roles;
     }
-
+    
     public void setRoles(List<Role> roles) {
         this.roles = roles;
     }
-
     
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }    
 
 }
