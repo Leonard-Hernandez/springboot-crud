f9c0dd67d5416a5a32eae9ad5b29d4ef8a64eabd
Leonard Hernandez <leonardhernandez02@gmail.com>
2025-04-09 20:52:43 -0500

    improve the methos save, with roles and password encrypted

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/services/UserServiceImpl.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.andres.curso.springboot.app.springboot_crud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.andres.curso.springboot.app.springboot_crud.entities.Role;
 import com.andres.curso.springboot.app.springboot_crud.entities.User;
 import com.andres.curso.springboot.app.springboot_crud.repositories.RoleRepository;
 import com.andres.curso.springboot.app.springboot_crud.repositories.UserRepository;
@@ -32,6 +35,19 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public User save(User user) {
+        List<Role> roles = new ArrayList<>();
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        optionalRoleUser.ifPresent(roles::add);
+
+        if (user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 
